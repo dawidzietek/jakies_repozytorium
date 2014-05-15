334e0a399f303c2df26ef15615c20f3a3a73bd95
Dawid <dawid.zietekk@gmail.com>
2014-05-15 17:10:37 +0200

    dodałem funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,5 +2,9 @@ def dodawanie():
     a = int(input('Pierwsza liczba'))
     b = int(input('Druga liczba'))
     print(a+b)
-    
+
+def get_info():
+    print("KALKULATOR")
+
+get_info()
 dodawanie()
